946630244f70282db91576905ec06cba47cee3b1
Ben D'Herville <ben.dherville@encompasscorporation.com>
2015-01-13 21:28:52 +1100

    Use default port

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var hapi = require('hapi');
 var routes = require('./routes');
 
 var server = new hapi.Server()
-server.connection({ address: '0.0.0.0' });
+server.connection({ address: '0.0.0.0', port: process.env.PORT });
 
 for (var i = 0; i < routes.length; i++) {
   server.route(routes[i]);
